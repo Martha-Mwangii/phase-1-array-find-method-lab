c322b68823a812c3936db630fd8246557859613b
Martha Wangui Mwangi <marthawm7252@gmail.com>
2024-11-29 20:40:45 +0300

    phase-1-array-find-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+const record = [
+    { year: "2018", result: "N/A"},
+    { year: "2017", result: "N/A"},
+    { year: "2016", result: "N/A"},
+    { year: "2015", result: "W"},
+  
+    { year: "1960", result: "N/A"}
+  ]
+  function superbowlWin(record){
+    let winResult = record.find(function(record){
+      return record.result ==="W";
+    })
+    if(winResult){
+      return winResult.year;
+      
+    }
+    
+  }
